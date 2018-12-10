93018b544171ab2bd54d05af074afc0e57c77e93
964453362@qq.com <964453362@qq.com>
2018-12-10 19:41:51 +0800

    rebuild

--- src/renderer/components/example/tabs.vue
+++ src/renderer/components/example/tabs.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <Tabs></Tabs>
+        <Tabs :data="tabsData" type="line"></Tabs>
+        <Tabs :data="tabsCardData" type="card" class="p-tabs-card"></Tabs>
     </div>
 </template>
 <script>
@@ -9,7 +10,46 @@ import {Tabs} from '../index';
 export default {
     components: {
         Tabs
+    },
+    data() {
+        return {
+            tabsData: [{
+                text: '个性推荐',
+                id: 1,
+                $active: true
+            },{
+                text: '歌曲',
+                id: 2,
+            },{
+                text: '主播电台',
+                id: 3,
+            },{
+                text: '排行榜',
+                id: 4,
+            },{
+                text: '歌手',
+                id: 5,
+            },{
+                text: '最新音乐',
+                id: 6,
+            }],
+            tabsCardData: [
+                {
+                    text: '播放列表',
+                    id: 1,
+                },{
+                    text: '历史记录',
+                    id: 2,
+                }
+            ]
+        }
     }
 }
 
 </script>
+
+<style lang="stylus">
+    .p-tabs-card{
+        margin-top: 20px;
+    }
+</style>
--- src/renderer/components/ui/tabs/view.vue
+++ src/renderer/components/ui/tabs/view.vue
@@ -6,12 +6,58 @@ const prefix = 'c-tabs';
 
 export default {
     props: {
+        data: {
+            type: Array,
+            default: () => [],
+        },
+        // card, line
+        type: {
+            type: String,
+            default: 'card'
+        }
+    },
+    data() {
+        return {
+            useSlotScope: false,
+            prefix,
+        }
+    },
+    methods: {
+        init() {
+            this.setDefaultState();
+            this.useSlotScope = this.$slotScope;
+        },
+        setDefaultState() {
+            this.data.forEach((item, idx) => {
+                this.$set(item, '$active', !!item.$active);
+            });
+        },
+        handleClick(item, idx) {
+            item.$active = !item.$active;
+            
+            this.data.forEach(el => {
+                if(item !== el) el.$active = false
+            });
+            this.$emit('change', item, idx);
+        }
+    },
+    mounted() {
+        this.init();
     },
     computed: {
         classes() {
             return [
                 `${prefix}`,
+                {[`${prefix}-${this.type}`]: this.type},
             ];
+        }
+    },
+    watch: {
+        data: {
+            handler() {
+                this.setDefaultState();
+            },
+            deep: true
         }
     }
 }
